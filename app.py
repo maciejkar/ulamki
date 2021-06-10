8c8a2b5abfa50eeeb1f1b159673636055d4bb1d4
Maciej Karczewski <m.karczewski2001@gmail.com>
2021-06-10 15:40:42 +0200

    Test

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, escape
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name=request.args.get("name","World")
+    return render_template("index.html", name=name)
+
+@app.route('/hello/<name>')
+def hello_Universe(name):
+    return render_template("index.html", name=escape(name))
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+source .venv/scripts/activate
+export FLASK_ENV=development
+export FLASK_APP=app.py
